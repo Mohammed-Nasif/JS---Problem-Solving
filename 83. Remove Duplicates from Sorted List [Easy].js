c5aad146945ac7eb28e7fbedfbca37e4339d137e
Mohammed Nasif <99568326+Mohammed-Nasif@users.noreply.github.com>
2022-10-26 22:52:57 +0200

    Update 83. Remove Duplicates from Sorted List [Easy].js

--- 83. Remove Duplicates from Sorted List [Easy].js
+++ 83. Remove Duplicates from Sorted List [Easy].js
@@ -9,6 +9,32 @@
  * @param {ListNode} head
  * @return {ListNode}
  */
+
+
+// 1st Method: Sorted or UnSorted
+var deleteDuplicates = function(head) {
+        let current = head;
+        let temp;
+        let prev;
+        let listSet = new Set();
+    
+        while(current){
+            if(listSet.has(current.val)){
+                prev.next = current.next;
+                delete(current);
+                current = prev.next;
+            }else{
+                prev = current;
+                listSet.add(current.val);
+                current = current.next;
+            }
+        }
+    
+    return head;
+};
+
+
+// 2nd Method: If Sorted List 
 var deleteDuplicates = function(head) {
         let current = head;
         let temp;
