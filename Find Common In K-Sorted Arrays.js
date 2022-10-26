d21bb37cf58e1b462f5fd07962215249613a8d7b
Mohammed Nasif <99568326+Mohammed-Nasif@users.noreply.github.com>
2022-10-26 02:11:42 +0200

    Create Find Common In K-Sorted Arrays.js
    Not LeetCode Problem

---
+++ Find Common In K-Sorted Arrays.js
@@ -0,0 +1,22 @@
+// Not LeetCode Problem
+
+
+/*
+Find common elements in  k-sorted arrays [[1 ,2 ,3 ],[1 ,2 ,3 ,4 ],[1 ,2 ]] The answers is [1,2]
+*/
+
+function commonInSortedArrays(inputArray){
+    const commonElements = [];
+    inputArray.sort((a,b)=> a.length - b.length);
+    const smallArr = inputArray[0];
+    const inputSet = inputArray.map((arr) => new Set(arr));
+    
+    smallArr.forEach((ele) => {
+        if(inputSet.every((arrSet) => arrSet.has(ele))){
+            commonElements.push(ele);
+        }
+    })
+    return commonElements;
+}
+
+console.log(commonInSortedArrays([[1 ,2 ,3 ],[1 ,2 ,3 ,4 ],[1 ,2 ]]))
