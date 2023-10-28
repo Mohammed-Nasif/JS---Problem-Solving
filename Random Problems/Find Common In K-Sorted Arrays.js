4bb36b22f64b90c90da268f6553de53e816a6f4c
Mohammed Nasif <99568326+Mohammed-Nasif@users.noreply.github.com>
2023-10-28 11:14:43 -0700

    Update Find Common In K-Sorted Arrays.js

--- Random Problems/Find Common In K-Sorted Arrays.js
+++ Random Problems/Find Common In K-Sorted Arrays.js
@@ -2,6 +2,7 @@
 Find common elements in  k-sorted arrays [[1 ,2 ,3 ],[1 ,2 ,3 ,4 ],[1 ,2 ]] The answers is [1,2]
 */
 
+// 1st Approach => Using Sets
 function commonInSortedArrays(inputArray){
     const commonElements = [];
     inputArray.sort((a,b)=> a.length - b.length);
@@ -14,6 +15,23 @@ function commonInSortedArrays(inputArray){
         }
     })
     return commonElements;
+}
+
+console.log(commonInSortedArrays([[1 ,2 ,3 ],[1 ,2 ,3 ,4 ],[1 ,2 ]]))
+
+
+// 2nd Approach => Using HashMap 
+function commonInSortedArrays(inputArray){
+    const inputSet = inputArray.map((arr) => new Set(arr));
+    const inputArrayFlatted = inputSet.map((set) => [...set]).flat();
+    
+    const hashMap = {};
+    
+    inputArrayFlatted.forEach((e) => {
+        hashMap[e] = (hashMap[e] + 1) || 1;
+    })
+    
+    return Object.entries(hashMap).filter((e) => e[1] === inputArray.length).map((e) => +e[0]);
 }
 
 console.log(commonInSortedArrays([[1 ,2 ,3 ],[1 ,2 ,3 ,4 ],[1 ,2 ]]))
